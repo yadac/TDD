910095f9e2c0cc3bfbbb568886e1e85c47baab6c
Yosuke Adachi <yosuke.adachi@avanade.com>
2024-08-14 20:32:46 +0900

    意図を語るテストまで

--- Model/Dollar.cs
+++ Model/Dollar.cs
@@ -9,19 +9,19 @@ namespace TDD.Model
     public class Dollar
     {
         private int _amount;
-        public int Amount
-        {
-            get { return _amount; }
-            set { _amount = value; }
-        }
 
         public Dollar(int amount)
         {
-            Amount = amount;
+            _amount = amount;
         }
         public Dollar Times(int multiplier)
         {
-            return new Dollar(Amount * multiplier);
+            return new Dollar(_amount * multiplier);
+        }
+        public override bool Equals(object? obj)
+        {
+            if (obj == null) return false;
+            return _amount == ((Dollar)obj)._amount;
         }
     }
 }
