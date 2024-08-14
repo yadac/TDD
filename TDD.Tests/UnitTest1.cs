6abb5939f409d34c7942443b1c0b34c23091d349
Yosuke Adachi <yosuke.adachi@avanade.com>
2024-08-14 20:42:35 +0900

    Gitでの管理対象誤りを修正

---
+++ TDD.Tests/UnitTest1.cs
@@ -0,0 +1,25 @@
+using TDD.Model;
+
+namespace TDD.Tests
+{
+    [TestClass]
+    public class MoneyTest
+    {
+        [TestMethod]
+        public void TestMultiplication()
+        {
+            Dollar five = new Dollar(5);
+            Assert.AreEqual(new Dollar(10), five.Times(2));
+            Assert.AreEqual(new Dollar(15), five.Times(3));
+        }
+        [TestMethod]
+        public void TestEquality()
+        {
+            Dollar one = new Dollar(5);
+            Dollar two = new Dollar(5);
+            Dollar three = new Dollar(6);
+            Assert.IsTrue(one.Equals(two));
+            Assert.IsFalse(one.Equals(three));
+        }
+    }
+}
