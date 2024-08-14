504cb65369ba1fd8f3670ef63a52f25b77811c4b
Yosuke Adachi <yosuke.adachi@avanade.com>
2024-08-14 10:54:09 +0900

    最初のテストが失敗するところまで

---
+++ Model/Dollar.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDD.Model
+{
+    public class Dollar
+    {
+        private int _amount;
+        public int Amount
+        {
+            get { return _amount; }
+        }
+
+        public Dollar(int amount)
+        {
+        }
+        public void Times(int multiplier)
+        {
+        }
+    }
+}
